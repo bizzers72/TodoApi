c370315997cfbd11369ca426b2c459bd1951d692
bizzers72 <pete.bisby@btinternet.com>
2024-05-08 13:05:10 +0100

    build: Added testing project, initial Task services

---
+++ ToDoApiTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ToDoApiTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TodoApi/Services/TasksService.cs
@@ -0,0 +1,28 @@
+﻿using TodoApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace TodoApi.Services;
+
+public class TasksService
+{
+    private readonly IMongoCollection<TaskItem> _tasksCollection;
+
+    public TasksService(IOptions<TaskStoreDatabaseSettings> taskStoreDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(taskStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(taskStoreDatabaseSettings.Value.DatabaseName);
+
+        _tasksCollection = mongoDatabase.GetCollection<TaskItem>(taskStoreDatabaseSettings.Value.TasksCollectionName);
+    }
+
+}
+
+
+
+
+
+    
+
+
