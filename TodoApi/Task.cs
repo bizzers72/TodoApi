db0edbef5663f391aeea936911789863e7fc4b84
bizzers72 <pete.bisby@btinternet.com>
2024-05-08 09:26:09 +0100

    Add Task class

---
+++ TodoApi/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete {  get; set; }
+    }
+}
